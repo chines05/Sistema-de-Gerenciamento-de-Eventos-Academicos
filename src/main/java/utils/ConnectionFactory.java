c921a004ade2a4a8ed60befba0ed7ae0a86dfbb6
chines05 <gpalmenara@gmail.com>
2025-07-07 20:36:36 -0300

    Feat: ConnectionFactory

--- src/main/java/utils/ConnectionFactory.java
+++ src/main/java/utils/ConnectionFactory.java
@@ -10,37 +10,137 @@ public class ConnectionFactory {
     private static final String URL = "jdbc:sqlite:src/main/resources/eventos_academicos.db";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(URL);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver JDBC não encontrado", e);
+        }
     }
 
     public static void criarTabelas() {
         String sqlUsuarios = """
-            CREATE TABLE IF NOT EXISTS Usuario (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,s
-                email TEXT UNIQUE NOT NULL,
-                password TEXT NOT NULL,
-                role TEXT CHECK(role IN ('ADMIN', 'ALUNO', 'PROFESSOR', 'PROFISSIONAL'))
-            );
-            """;
+        CREATE TABLE IF NOT EXISTS User (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            senha TEXT NOT NULL,
+            role TEXT CHECK(role IN ('ADMIN', 'ALUNO', 'PROFESSOR', 'PROFISSIONAL')),
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        """;
 
         String sqlEventos = """
-            CREATE TABLE IF NOT EXISTS Evento (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                description TEXT,
-                data_inicio TEXT NOT NULL,
-                data_fim TEXT NOT NULL
-            );
-            """;
+        CREATE TABLE IF NOT EXISTS Evento (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            descricao TEXT,
+            data_inicio DATETIME NOT NULL,
+            data_fim DATETIME NOT NULL,
+            vagas_total INTEGER,
+            vagas_disponivel INTEGER
+        );
+        """;
 
-        try (Connection conn = getConnection();
-             Statement stmt = conn.createStatement()) {
+        String sqlEvento_user = """
+        CREATE TABLE IF NOT EXISTS evento_user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            usuario_id INTEGER NOT NULL,
+            evento_id INTEGER NOT NULL,
+            data_inscricao DATETIME DEFAULT CURRENT_TIMESTAMP,
+            status_pagamento TEXT DEFAULT 'PENDENTE',
+            FOREIGN KEY (usuario_id) REFERENCES User(id),
+            FOREIGN KEY (evento_id) REFERENCES Evento(id),
+            UNIQUE (usuario_id, evento_id)
+        );
+        """;
+
+        String sqlAtividade = """
+        CREATE TABLE IF NOT EXISTS Atividade (    
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            evento_id INTEGER NOT NULL,
+            nome TEXT NOT NULL,
+            descricao TEXT,
+            data_realizacao DATETIME NOT NULL,
+            hora_inicio TIME NOT NULL,
+            hora_fim TIME NOT NULL,
+            limite_inscritos INTEGER,
+            vagas_disponiveis INTEGER,
+            tipo TEXT CHECK(tipo IN ('PALESTRA', 'SIMPOSIO', 'CURSO')),
+            FOREIGN KEY (evento_id) REFERENCES Evento(id)
+        );
+        """;
+
+        String sqlConfigInscricao = """
+        CREATE TABLE IF NOT EXISTS config_inscricao (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            role TEXT NOT NULL UNIQUE CHECK(role IN ('ALUNO', 'PROFESSOR', 'PROFISSIONAL')),
+            valor REAL NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        """;
+
+        String sqlInsertValores = """
+        INSERT OR IGNORE INTO config_inscricao (role, valor) VALUES 
+            ('ALUNO', 10.0),
+            ('PROFESSOR', 50.0),
+            ('PROFISSIONAL', 100.0);
+        """;
+
+        String deleteUsers = """
+        DROP TABLE IF EXISTS User;
+        """;
+
+        String deleteEventos = """
+        DROP TABLE IF EXISTS Evento;
+        """;
+
+        String deleteEvento_user = """
+        DROP TABLE IF EXISTS evento_user;
+        """;
+
+        String deleteAtividade = """
+        DROP TABLE IF EXISTS Atividade;
+        """;
+
+        String deleteConfigInscricao = """
+        DROP TABLE IF EXISTS config_inscricao;
+        """;
+
+        try  {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+
             stmt.execute(sqlUsuarios);
             stmt.execute(sqlEventos);
+            stmt.execute(sqlEvento_user);
+            stmt.execute(sqlAtividade);
+
+            stmt.execute(sqlConfigInscricao);
+            stmt.execute(sqlInsertValores);
+
+//            stmt.execute(deleteUsers);
+//            stmt.execute(deleteEventos);
+//            stmt.execute(deleteEvento_user);
+//            stmt.execute(deleteAtividade);
+//            stmt.execute(deleteConfigInscricao);
+
             System.out.println("Tabelas criadas com sucesso!");
         } catch (SQLException e) {
             System.err.println("Erro ao criar tabelas: " + e.getMessage());
+        } finally {
+            closeConnection(null);
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao fechar a conexão: " + e.getMessage());
         }
     }
 
